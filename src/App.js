08ea97d5b28519719abb15cab37fd62ffa25c8dd
msjonty <senapatimrutunjaya59@gmail.com>
2023-11-26 04:23:02 +0530

    error solved

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="root">I am learning React. My life is getting better.</div>
+      
+    </div>
+  );
+}
+
+export default App;
